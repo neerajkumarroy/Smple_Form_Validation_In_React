db0460a0eb15dc7d21707c682dea63e38c843a9a
Neeraj Kumar <148259510+neerajkumarroy@users.noreply.github.com>
2023-11-24 16:21:07 +0530

    oo

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import User from './User';
+
+function App() {
+
+  return (
+    <div className="App">
+      <User />
+
+    </div>
+
+  )
+}
+export default App;
